1e3826952746670f2b9884db5c2415b0dadd4cbb
marciano02 <148990684+marciano02@users.noreply.github.com>
2024-05-27 08:49:42 -0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,12 @@
+genio1 = int(input("Digite o primeiro número: "))
+genio2 = int(input("Digite o segundo número: "))
+
+soma = genio1 + genio2
+diferença = genio1 - genio2
+multi = genio1 * genio2
+divisão = genio1 / genio2
+
+print(f"A soma é: {soma}")
+print(f"A diferença é: {diferença}")
+print(f"A Multiplicação é: {multi}")
+print(f"A divisão é: {divisão}")
